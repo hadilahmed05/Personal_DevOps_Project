9f80854de7a8fb361a42f528a74a9172d8227c33
Hadil Ahmed <hadil.ahmed@insat.ucar.tn>
2024-08-17 20:24:35 +0100

    code update

--- StudentCrudApplicationSpringBoot/src/test/java/com/arnab/studentcrud/student/StudentRestControllerTest.java
+++ StudentCrudApplicationSpringBoot/src/test/java/com/arnab/studentcrud/student/StudentRestControllerTest.java
@@ -14,65 +14,53 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)
-
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class StudentRestControllerTest {
+
     @LocalServerPort
     private int port;
+
     @Autowired
     private TestRestTemplate restTemplate;
 
-    StudentRestControllerTest() {
-    }
-
-    public ResponseEntity<String> doRestCall(String uri, MultiValueMap<String, String> querryParams, MultiValueMap<String, String> headers, String body, Map<String, String> pathParams, HttpMethod method) {
+    private ResponseEntity<String> doRestCall(String uri, MultiValueMap<String, String> queryParams, MultiValueMap<String, String> headers, String body, Map<String, String> pathParams, HttpMethod method) {
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(uri);
-        HttpEntity<String> requestEntity = new HttpEntity(body, headers);
+        if (queryParams != null) {
+            builder.queryParams(queryParams);
+        }
+        HttpEntity<String> requestEntity = new HttpEntity<>(body, headers);
         ResponseEntity<String> responseEntity = this.restTemplate.exchange(builder.buildAndExpand(pathParams).toUri(), method, requestEntity, String.class);
         return responseEntity;
     }
 
     @Test
     void getAllStudents() {
-        String url = "http://localhost:" + this.port;
-        Map<String, String> pathVariables = new HashMap();
-        HttpEntity<String> entity = new HttpEntity((Object)null, (MultiValueMap)null);
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url);
-        ResponseEntity<String> response = this.restTemplate.exchange(builder.buildAndExpand(pathVariables).toUri(), HttpMethod.GET, entity, String.class);
-        System.out.println((String)response.getBody());
-        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        String url = "http://localhost:" + this.port + "/students";
+        ResponseEntity<String> response = this.restTemplate.getForEntity(url, String.class);
+        System.out.println(response.getBody());
+        
     }
 
     @Test
     void addStudent() {
         String url = "http://localhost:" + this.port + "/students";
         String body = "{\"name\":\"John\",\"grp\":\"SQA\",\"age\":25}";
-        MultiValueMap<String, String> headers = new LinkedMultiValueMap();
-        List<String> al = new ArrayList();
-        al.add("application/json");
-        headers.put("content-type", al);
-        Map<String, String> pathParams = new HashMap();
-        ResponseEntity<String> response = this.doRestCall(url, (MultiValueMap)null, headers, body, pathParams, HttpMethod.POST);
-        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        headers.add("Content-Type", "application/json");
+        ResponseEntity<String> response = this.doRestCall(url, null, headers, body, new HashMap<>(), HttpMethod.POST);
+        
     }
 
     @Test
     void getStudentById() {
         String url = "http://localhost:" + this.port + "/students/{id}";
-        Map<String, String> pathVariables = new HashMap();
+        Map<String, String> pathVariables = new HashMap<>();
         pathVariables.put("id", "14");
-        HttpEntity<String> entity = new HttpEntity((Object)null, (MultiValueMap)null);
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url);
-        ResponseEntity<String> response = this.restTemplate.exchange(builder.buildAndExpand(pathVariables).toUri(), HttpMethod.GET, entity, String.class);
-        System.out.println((String)response.getBody());
+        ResponseEntity<String> response = this.restTemplate.getForEntity(url, String.class, pathVariables);
+        System.out.println(response.getBody());
         Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
@@ -80,28 +68,20 @@ class StudentRestControllerTest {
     void updateStudent() {
         String url = "http://localhost:" + this.port + "/students/{id}";
         String body = "{\"name\":\"John\",\"grp\":\"ML\",\"age\":25}";
-        MultiValueMap<String, String> headers = new LinkedMultiValueMap();
-        List<String> al = new ArrayList();
-        al.add("application/json");
-        headers.put("content-type", al);
-        Map<String, String> pathParams = new HashMap();
-        pathParams.put("id", "14");
-        ResponseEntity<String> response = this.doRestCall(url, (MultiValueMap)null, headers, body, pathParams, HttpMethod.PUT);
-        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        headers.add("Content-Type", "application/json");
+        Map<String, String> pathVariables = new HashMap<>();
+        pathVariables.put("id", "14");
+        ResponseEntity<String> response = this.doRestCall(url, null, headers, body, pathVariables, HttpMethod.PUT);
+        
     }
 
     @Test
     void deleteStudentById() {
         String url = "http://localhost:" + this.port + "/students/{id}";
-        String body = "{\"name\":\"John\",\"grp\":\"ML\",\"age\":25}";
-        MultiValueMap<String, String> headers = new LinkedMultiValueMap();
-        List<String> al = new ArrayList();
-        al.add("application/json");
-        headers.put("content-type", al);
-        Map<String, String> pathParams = new HashMap();
-        pathParams.put("id", "18");
-        ResponseEntity<String> response = this.doRestCall(url, (MultiValueMap)null, headers, body, pathParams, HttpMethod.DELETE);
-        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Map<String, String> pathVariables = new HashMap<>();
+        pathVariables.put("id", "18");
+        ResponseEntity<String> response = this.restTemplate.exchange(url, HttpMethod.DELETE, null, String.class, pathVariables);
+        
     }
-
 }
